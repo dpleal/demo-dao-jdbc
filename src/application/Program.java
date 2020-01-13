7bdc1bfc3bef70b08b595542969aba674c4d2114
Denis Leal <denisleal@msn.com>
2020-01-13 13:55:45 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Departament obj = new Departament(1,"Books");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
 		
-		
-		System.out.println(obj);
+		System.out.println(seller);
 	}
 
 }
