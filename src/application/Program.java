c47603b625ea115733c8b642c68246d4ab1596c1
Denis Leal <denisleal@msn.com>
2020-01-13 13:44:48 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Departament;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Departament obj = new Departament(1,"Books");
+		
+		
+		System.out.println(obj);
+	}
+
+}
